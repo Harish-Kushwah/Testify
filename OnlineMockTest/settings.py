e8ab9e75c85dab0326e3f8a2ab60aa256215310b
Harish-Kushwah <harishkushwah54321@gmail.com>
2024-09-05 23:02:18 +0530

    database updated

--- OnlineMockTest/settings.py
+++ OnlineMockTest/settings.py
@@ -10,10 +10,15 @@ print(BASE_DIR)
 
 
 #NOTE:MEDIA FILE STORING IN cloudinary
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Testify',
+         'USER':'postgres',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
